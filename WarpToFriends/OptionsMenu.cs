cca280f1584501a7875132046231fe3b556f915f
Jonathan Goodman <jgoodman@algomau.ca>
2019-07-20 19:58:45 -0400

    Options Confign Menu, Buttons are now SButton Class - not string

--- WarpToFriends/OptionsMenu.cs
+++ WarpToFriends/OptionsMenu.cs
@@ -60,7 +60,7 @@ namespace WarpToFriends
 		{
 			var property = _config.GetType().GetProperties().FirstOrDefault(p => p.Name == _property);
 			if(property != null)
-				property.SetValue(_config, key.ToString());
+				property.SetValue(_config, key.ToSButton());
 
 			exitThisMenu(true);
 			Game1.activeClickableMenu = _returnMenu;
@@ -149,6 +149,11 @@ namespace WarpToFriends
 					DrawOption(idx, displayName, b);
 
 					DrawBoolOption(idx, (bool)value, option.Name, b);
+				}
+				else if (optionType == typeof(SButton))
+				{
+					DrawOption(idx, displayName, b, value.ToString());
+					DrawKeyOption(idx, ((SButton)value).ToString(), option.Name, b);
 				}
 				else if(optionType == typeof(string))
 				{
