791f9aa7e096e248a1a018f4b800815f0f509ff7
Nightly <Nightlyyyy@hotmail.com>
2019-12-03 21:14:57 +0100

    Updates for smapi 3.0 compatibility with stardew update 1.14

--- WarpToFriends/ChatReader.cs
+++ WarpToFriends/ChatReader.cs
@@ -92,7 +92,7 @@ namespace WarpToFriends
 			if (_lastMessage != _chatMessages.Last())
 			{
 				_lastMessage = _chatMessages.Last();
-				string msg = ChatMessage.makeMessagePlaintext(_lastMessage.message);
+				string msg = ChatMessage.makeMessagePlaintext(_lastMessage.message , true);
 
 				NewChatMessageEvent(null, new NewChatMessageEvent(msg));
 
